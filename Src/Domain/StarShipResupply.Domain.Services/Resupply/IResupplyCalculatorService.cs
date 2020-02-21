82ad5c01f4155d836494b7d5d77b48f89e55dfd0
onofre.junior <onofre.junior@farfetch.com>
2020-02-21 19:12:25 +0000

    feat: Created resupply calculator service.

--- Src/Domain/StarShipResupply.Domain.Services/Resupply/IResupplyCalculatorService.cs
+++ Src/Domain/StarShipResupply.Domain.Services/Resupply/IResupplyCalculatorService.cs
@@ -2,8 +2,17 @@
 {
     using StarshipResupply.Domain.Model.Starship;
 
+    /// <summary>
+    /// Represents the interface with the method declaration for the resupply calculator service.
+    /// </summary>
     public interface IResupplyCalculatorService
     {
+        /// <summary>
+        /// Represents the method definition to calculate the specified megalights.
+        /// </summary>
+        /// <param name="megalights">The megalights.</param>
+        /// <param name="starship">The starship.</param>
+        /// <returns>An <see cref="int"/> value.</returns>
         string Calculate(decimal megalights, Starship starship);
     }
 }
--- Src/Domain/StarShipResupply.Domain.Services/Resupply/ResupplyCalculatorService.cs
+++ Src/Domain/StarShipResupply.Domain.Services/Resupply/ResupplyCalculatorService.cs
@@ -2,67 +2,88 @@
 {
     using StarshipResupply.Domain.Model.Starship;
     using System;
+    using System.Linq;
 
     /// <summary>
-    /// many stops for resupply are required.
+    /// This class represents the service that calculates how many stops for resupply are required.
     /// </summary>
-    internal sealed class ResupplyCalculatorService : IResupplyCalculatorService
+    public class ResupplyCalculatorService : IResupplyCalculatorService
     {
         private const string Day = "day";
+        private const int HoursInDay = 24;
+        private const int HoursInMonth = 720;
+        private const int HoursInWeek = 168;
+        private const int HoursInYear = 8760;
         private const string Month = "month";
         private const string UnknownMegalights = "Unknown";
         private const int Unkwnow = 0;
         private const string Week = "week";
         private const string Year = "year";
 
+        /// <summary>
+        /// Calculates the specified megalights.
+        /// </summary>
+        /// <param name="megalights">The megalights.</param>
+        /// <param name="starship">The starship.</param>
+        /// <returns>An <see cref="int"/> value.</returns>
         public string Calculate(decimal megalights, Starship starship)
         {
             var starshipMegalights = GetStarshipMegalights(starship.Mglt);
             var hours = ConvertConsumablesInHours(starship.Consumables);
 
-            if (starshipMegalights <= 0)
+            if (starshipMegalights == default)
             {
                 return UnknownMegalights;
             }
 
-            var numberOfStops = Math.Round(hours * starshipMegalights / megalights);
+            var numberOfStops = Math.Truncate(megalights / (hours * starshipMegalights));
 
             return numberOfStops.ToString();
         }
 
+        /// <summary>
+        /// Converts the consumables in hours.
+        /// </summary>
+        /// <param name="consumables">The consumables.</param>
+        /// <returns>An <see cref="int"/> value.</returns>
         private int ConvertConsumablesInHours(string consumables)
         {
             var consumableValues = consumables.Split();
 
-            if (consumableValues.Length <= 1)
+            if (consumableValues.First().Equals(consumableValues.Last()))
             {
                 return Unkwnow;
             }
 
-            var numberOfDays = GetNumberOfDays(consumableValues[1].ToLower());
+            var numberOfHours = GetNumberOfHours(consumableValues.Last().ToLower());
 
-            int.TryParse(consumableValues[0], out int number);
+            int.TryParse(consumableValues.First(), out int number);
 
-            return number * numberOfDays;
+            return number * numberOfHours;
         }
 
-        private int GetNumberOfDays(string period)
+        /// <summary>
+        /// Gets the number of hours within a period.
+        /// </summary>
+        /// <param name="period">The period.</param>
+        /// <returns>An <see cref="int"/> value.</returns>
+        private int GetNumberOfHours(string period)
         {
             if (period.Contains(Day))
             {
-                return 24;
+                return HoursInDay;
             }
             else if (period.Contains(Week))
             {
-                return 168;
+                return HoursInWeek;
             }
             else if (period.Contains(Month))
             {
-                return 720;
+                return HoursInMonth;
             }
             else if (period.Contains(Year))
             {
-                return 8760;
+                return HoursInYear;
             }
             else
             {
@@ -70,6 +91,11 @@
             }
         }
 
+        /// <summary>
+        /// Gets the starship megalights.
+        /// </summary>
+        /// <param name="mglt">The megalights.</param>
+        /// <returns>An <see cref="int"/> value.</returns>
         private int GetStarshipMegalights(string mglt)
         {
             int.TryParse(mglt, out int result);
