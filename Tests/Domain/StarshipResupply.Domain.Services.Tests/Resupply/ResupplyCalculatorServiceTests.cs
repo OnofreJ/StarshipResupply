8606e8594603844065abcaf86b37c625c3ea1118
onofrej <onofre.junior@gmail.com>
2020-02-24 10:59:25 +0000

    chore: Created unit tests.

---
+++ Tests/Domain/StarshipResupply.Domain.Services.Tests/Resupply/ResupplyCalculatorServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarshipResupply.Domain.Services.Tests.Resupply
+{
+    class ResupplyCalculatorServiceTests
+    {
+    }
+}
